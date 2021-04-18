e5f0d55f7c09f4a7d4e17f882d7c2b567187b435
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-04-18 15:36:47 +0300

    18/04/21#1

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const app = express();
-const port =8000;
+// const port =8000;
 const usersRoute = require('./routes/users.route');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(cors());
 app.use('/api/users',usersRoute);
 
-app.listen(port,()=>{
-    console.log(`application start at ${port}`)
-})
+// app.listen(port,()=>{
+//     console.log(`application start at ${port}`)
+// })
+app.listen(process.env.PORT || 5000);
